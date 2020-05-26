9aec735bab7e65be00390285686ecc79de9fc787
刘昊宸 <liuhaochen@s-MacBook-puro.local>
2020-05-26 22:16:36 +0800

    upload code

--- model/train.py
+++ model/train.py
@@ -62,7 +62,6 @@ if torch.cuda.is_available():
     print('torch.cuda:    ', torch.cuda.is_available(), torch.cuda.current_device(), torch.cuda.device_count(), torch.cuda.get_device_name(0), torch.cuda.device(torch.cuda.current_device()))
 else:
     print('GPU is not available!!!')
-print('random_seed:    ', random_seed)
 print('Train_Size:    ', Train_Size)
 print('Test_Size:     ', Test_Size)
 print('Emb_Size:      ', Emb_Size)
